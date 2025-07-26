f11f5c0a53e830e4152dca833720a06c683a51cf
Jerry Zhou <jerry.zhou25@gmail.com>
2025-07-26 16:19:27 +1000

    Shadcn components setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
-  return <>Milkkostore</>;
+  return <Button variant="outline">Button</Button>;
 }
  
 export default HomePage;
