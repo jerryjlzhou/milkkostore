9558717671692e173fc9abc786a6417a774f5a83
Jerry Zhou <jerry.zhou25@gmail.com>
2025-07-27 18:51:35 +1000

    Added not found page custom

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -13,6 +13,19 @@ const NotFoundPage = () => {
         alt={`${APP_NAME} logo`}
         priority={true}
       />
+      <div className="p-6 w-1/3 rounded-lg shadow-md text-center">
+        <h1 className="text-3xl font-bold" mb-4>
+          Not Found
+        </h1>
+        <p className="text-destructive">Could not find requested page</p>
+        <Button
+          variant="outline"
+          className="mt-4 ml-2"
+          onClick={() => (window.location.href = '/')}
+        >
+          Go to Home
+        </Button>
+      </div>
     </div>
   );
 };
