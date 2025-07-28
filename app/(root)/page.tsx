551a69f898c4401c32340ef0d02c607500c52ee4
Jerry Zhou <jerry.zhou25@gmail.com>
2025-07-28 16:25:29 +1000

    Implemented database loading of products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,14 @@
 import sampleData from '@/sample-data';
 import ProductList from '@/components/shared/product/product-list';
+import { getLatestProducts } from '@/lib/actions/product.actions';
+
 
 const HomePage = async () => {
+  const latestProducts = await getLatestProducts();
   return (
     <>
       <ProductList
-        data={sampleData.products}
+        data={latestProducts}
         title="Newest Arrivals"
         limit={4}
       />
