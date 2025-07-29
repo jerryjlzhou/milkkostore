c3b9c5676eeb87679c7cdc0e5781cabbfadb6d95
Jerry Zhou <jerry.zhou25@gmail.com>
2025-07-30 01:37:07 +1000

    Product images component

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -4,6 +4,8 @@ import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import ProductPrice from '@/components/shared/product/product-price';
+import ProductImages from '@/components/shared/product/product-images';
+
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -17,55 +19,57 @@ const ProductDetailsPage = async (props: {
       <section>
         <div className="grid grid-cols-1 md:grid-cols-5 gap-6">
           {/* Images column */}
-          <div className="col-span-2">{/* Images Component */}</div>
+          <div className="col-span-2">
+            <ProductImages images={product.images}/>
+          </div>
           {/* Details Column */}
           <div className="col-span-2 p-5">
-          <div className="flex flex-col gap-6">
-            <p>
-              {product.brand} {product.category}
-            </p>
-            <h1 className="h3-bold">{product.name}</h1>
-            <p>
-              {product.rating} of {product.numReviews} Reviews
-            </p>
-            <div className="flex flex-col sm:flex-row sm:items-center">
-              <ProductPrice
-                value={Number(product.price)}
-                className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2"
-              />
+            <div className="flex flex-col gap-6">
+              <p>
+                {product.brand} {product.category}
+              </p>
+              <h1 className="h3-bold">{product.name}</h1>
+              <p>
+                {product.rating} of {product.numReviews} Reviews
+              </p>
+              <div className="flex flex-col sm:flex-row sm:items-center">
+                <ProductPrice
+                  value={Number(product.price)}
+                  className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2"
+                />
+              </div>
+            </div>
+            <div className="mt-10">
+              <p className="font-semibold">Description</p>
+              <p>{product.description}</p>
             </div>
           </div>
-          <div className="mt-10">
-            <p className="font-semibold">Description</p>
-            <p>{product.description}</p>
-          </div>
-        </div>
-        {/* Action column */}
-        <div className="col-span-1">
-          <Card>
-            <CardContent className='p-4'>
-              <div className="mb-2 flex justify-between">
-                <div className="mr-2">Price</div>
-                <div>
-                  <ProductPrice value={Number(product.price)}/>
+          {/* Action column */}
+          <div className="col-span-1">
+            <Card>
+              <CardContent className="p-4">
+                <div className="mb-2 flex justify-between">
+                  <div className="mr-2">Price</div>
+                  <div>
+                    <ProductPrice value={Number(product.price)} />
+                  </div>
                 </div>
-              </div>
-              <div className="mb-2 flex justify-between">
-                <div className="mr-1">Status</div>
-                {product.stock > 0 ? (
-                  <Badge variant='outline'>In Stock</Badge> 
-                ) : (
-                  <Badge variant="destructive">Out of Stock</Badge>
+                <div className="mb-2 flex justify-between">
+                  <div className="mr-1">Status</div>
+                  {product.stock > 0 ? (
+                    <Badge variant="outline">In Stock</Badge>
+                  ) : (
+                    <Badge variant="destructive">Out of Stock</Badge>
+                  )}
+                </div>
+                {product.stock > 0 && (
+                  <div className="flex-center">
+                    <Button className="w-full">Add To Cart</Button>
+                  </div>
                 )}
-              </div>
-              {product.stock > 0 && (
-                <div className="flex-center">
-                  <Button className='w-full'>Add To Cart</Button>
-                </div>
-              )}
-            </CardContent>
-          </Card>
-        </div>
+              </CardContent>
+            </Card>
+          </div>
         </div>
       </section>
     </>
