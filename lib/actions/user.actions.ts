9dde2ab2d52b859f6487f7b1e46bca244077acab
Jerry Zhou <jerry.zhou25@gmail.com>
2025-07-31 00:29:42 +1000

    Fixed import

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,7 +5,6 @@ import { signIn, signOut } from '@/auth';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
 import { hashSync } from 'bcrypt-ts-edge';
 import { prisma } from '@/db/prisma';
-import { hash } from 'crypto';
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
