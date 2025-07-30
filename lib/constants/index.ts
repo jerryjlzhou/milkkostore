1cf8f862ffe8f53d794b161869dd37fcac4003fe
Jerry Zhou <jerry.zhou25@gmail.com>
2025-07-31 01:23:51 +1000

    Added sign up form

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -6,9 +6,14 @@ export const SERVER_URL =
 export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 
-
-
 export const signInDefaultValues = {
   email: '',
-  password: ''
+  password: '',
+};
+
+export const signUpDefaultValues = {
+  name: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
 };
