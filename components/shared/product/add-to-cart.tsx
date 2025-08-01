d860dd56b49f600b812c77844e14060292988972
Jerry Zhou <jerry.zhou25@gmail.com>
2025-08-01 15:34:06 +1000

    Add to cart component

---
+++ components/shared/product/add-to-cart.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { CartItem } from '@/types';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+import { Plus } from 'lucide-react';
+import { toast } from 'sonner';
+import { addItemToCart } from '@/lib/actions/cart.actions';
+
+const AddToCart = ({ item }: { item: CartItem }) => {
+  const router = useRouter();
+
+  const handleAddToCart = async () => {
+    const res = await addItemToCart(item);
+
+    if (!res.success) {
+      toast.error(res.message);
+      return;
+    }
+
+    // Handle success add to cart
+    toast.success(`${item.name} added to cart`, {
+      action: {
+        label: 'Go To Cart',
+        onClick: () => router.push('/cart'),
+      },
+      actionButtonStyle: {
+        backgroundColor: 'hsl(var(--primary))',
+        color: 'hsl(var(--primary-foreground))',
+      },
+      className:
+        'group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
+    });
+  };
+
+  return (
+    <Button className="w-full" type="button" onClick={handleAddToCart}>
+      <Plus />
+      Add to Cart
+    </Button>
+  );
+};
+
+export default AddToCart;
