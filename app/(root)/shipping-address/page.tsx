d31fa26a26e655d436cca9d21d42e056bb2e6c77
Jerry Zhou <jerry.zhou25@gmail.com>
2025-08-04 00:36:01 +1000

    Checkout steps

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -5,8 +5,7 @@ import { redirect } from 'next/navigation';
 import { ShippingAddress } from '@/types';
 import { getUserById } from '@/lib/actions/user.actions';
 import ShippingAddressForm from './shipping-address-form';
-
-
+import CheckoutSteps from '@/components/shared/checkout-steps';
 
 export const metadata: Metadata = {
   title: 'Shipping Address',
@@ -27,11 +26,10 @@ const ShippingAddressPage = async () => {
 
   const user = await getUserById(userId);
 
-
-
   return (
     <>
-      <ShippingAddressForm address = { user.address as ShippingAddress } />
+      <CheckoutSteps current={1} />
+      <ShippingAddressForm address={user.address as ShippingAddress} />
     </>
   );
 };
