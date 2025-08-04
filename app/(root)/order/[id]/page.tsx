24aede893fa794e8a6b5088df97f91237b3d9164
Jerry Zhou <jerry.zhou25@gmail.com>
2025-08-05 02:19:32 +1000

    Fixed minor bugs

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -1,40 +1,5 @@
-import { auth } from '@/auth';
-import { redirect } from 'next/navigation';
-import { Metadata } from 'next';
-
-// This is a placeholder for the order page that needs to be implemented
-// When implementing, make sure to add the security check
-
-export const metadata: Metadata = {
-  title: 'Order Details',
-};
-
-const OrderPage = async ({ params }: { params: { id: string } }) => {
-  const session = await auth();
-  
-  if (!session?.user?.id) {
-    return redirect('/sign-in');
-  }
-
-  // TODO: Implement order fetching logic
-  // const order = await getOrderById(params.id);
-  
-  // if (!order) {
-  //   return notFound();
-  // }
-
-  // Security check: Redirect if user doesn't own the order
-  // if (order.userId !== session?.user.id && session?.user.role !== 'admin') {
-  //   return redirect('/unauthorized');
-  // }
-
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Order Details</h1>
-      <p>Order ID: {params.id}</p>
-      {/* TODO: Implement order details display */}
-    </div>
-  );
-};
-
+const OrderPage = () => {
+  return <> Order Page </>;
+}
+ 
 export default OrderPage;
