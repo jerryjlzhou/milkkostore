12110202efc63610b34d16083051f33279149bd4
Jerry Zhou <jerry.zhou25@gmail.com>
2025-08-20 13:20:11 +1000

    Create order and capture payment request

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,6 +1,45 @@
 const base = process.env.PAYPAL_API_URL || 'https://api-m.sandbox.paypal.com';
 
-export const paypal = {};
+export const paypal = {
+  createOrder: async function createOrder(price: number) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders`;
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        intent: 'CAPTURE',
+        purchase_units: [
+          {
+            amount: {
+              currency_code: 'AUD',
+              value: price,
+            },
+          },
+        ],
+      }),
+    });
+    return handleResponse(response);
+  },
+  capturePayment: async function capturePayment(orderId: string) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'applications/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    return handleResponse(response);
+  },
+};
 
 // Generate paypal access token
 async function generateAccessToken() {
@@ -18,15 +57,17 @@ async function generateAccessToken() {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
   });
+  const jsonData = await handleResponse(response);
+  return jsonData.access_token;
+}
 
+async function handleResponse(response: Response) {
   if (response.ok) {
-    const jsonData = await response.json();
-    return jsonData.access_token;
+    return response.json();
   } else {
     const errorMessage = await response.text();
     throw new Error(errorMessage);
   }
 }
-
 
 export { generateAccessToken };
